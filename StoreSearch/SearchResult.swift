b68f3023e95ab98a7da6c183988270980a62e75f
Uriel Escorcia <uris_ec@hotmail.com>
2016-03-29 14:44:25 -0600

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Urisito on 3/29/16.
+//  Copyright © 2016 Urisito. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    
+    var name = ""
+    var artistName = ""
+}
